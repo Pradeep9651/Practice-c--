2fd063e8065020b0b2b2c101bc1c4cd48ce5cd2c
Pradeep Yadav <104863759+Pradeep9651@users.noreply.github.com>
2024-06-18 13:22:13 +0530

    bhhh

--- prime!notprime.cpp
+++ prime!notprime.cpp
@@ -3,13 +3,10 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    
-
     int i=2;
     while(i<n){
         if(n%i==0){
             cout<<"not prime for" << i <<endl;
-
         }
         else{
             cout<<"prime for"<<i<<endl;
